98d3d0c8cf09af7e9ec341b3b831fe025db0f54f
Godwin <49465931+red11scout@users.noreply.github.com>
2021-01-30 20:34:43 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,12 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Sheep Dogs</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+           Purpose driven fellowship centered on authentic connections and postive community impacts 
         </p>
       </div>
     </div>
@@ -32,10 +27,10 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('work')
+              props.onOpenArticle('gatherings')
             }}
           >
-            Work
+            Gatherings
           </button>
         </li>
         <li>
